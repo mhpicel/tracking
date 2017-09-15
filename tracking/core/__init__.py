c3d071e4a84f10c4d03188665f077abd6dff3826
Mark Picel <mhpicel@gmail.com>
2017-09-15 15:08:45 -0500

    Adjusted to accomodate restructuring

--- tracking/core/__init__.py
+++ tracking/core/__init__.py
@@ -13,6 +13,8 @@ TITAN cell tracking
 
 """
 
-from .cell_tracking import Cell_tracks
+#from .cell_tracking import Cell_tracks
+from .tracks import Cell_tracks
+from .visualization import animate
 
 __all__ = [s for s in dir() if not s.startswith('_')]
